5896b17e1f803db5abb75a07edbd4a185a4a0973
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-03-01 19:21:12 -0500

    [rebase] main with about-section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,5 +6,6 @@
 import SpurLogo from "./spurLogo.svg";
 import SpurBuilding from "./spurBuilding.png";
 import xLogo from "./xLogo.svg";
+import HalfEllipse from "./halfEllipse.svg";
 
-export { SpurLogo, SpurBuilding, xLogo };
+export { SpurLogo, SpurBuilding, xLogo, HalfEllipse };
