58d9b75eff4502d396021f78f52244a342d3cbf7
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-03-05 16:47:38 -0500

    [update] map cards dynamically and design cards

--- src/components/sections/Resources.section.tsx
+++ src/components/sections/Resources.section.tsx
@@ -1,7 +1,27 @@
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
+
 const ResourcesSection: React.FC = () => {
+  const cardData = [
+    {
+      title: "Investments & Funding",
+      description:
+        "Placerat maecenas aliquam primis duis viverra integer. Vehicula nulla bibendum facilisis per quis vehicula risus donec euismod.",
+    },
+    {
+      title: "AI-Driven Data Centers",
+      description:
+        "Placerat maecenas aliquam primis duis viverra integer. Vehicula nulla bibendum facilisis per quis vehicula risus donec euismod.",
+    },
+    {
+      title: "Office Spaces",
+      description:
+        "Placerat maecenas aliquam primis duis viverra integer. Vehicula nulla bibendum facilisis per quis vehicula risus donec euismod.",
+    },
+  ];
+
   return (
     <div>
-      <section className="relative h-screen bg-[#050B10] to-10%">
+      <section className="relative h-screen bg-[#0B1219] to-10%">
         <div className="absolute top-1/5 left-1/2 transform -translate-x-1/2 flex flex-col items-center text-6xl">
           <div className="flex gap-5 leading-relaxed">
             <h1 className="text-gradient">Catalysts</h1>
@@ -16,40 +36,20 @@ const ResourcesSection: React.FC = () => {
             convallis nostra vel dui diam. Curabitur ex nunc quis sit.{" "}
           </p>
         </div>
-        {/* Cards */}
+        {/* Cards (will be converted into a component) */}
         <div className="absolute top-3/5 left-1/2 transform -translate-x-1/2 w-2/3 flex gap-10">
-          <div className="border-1 border-amber-500 rounded-4xl p-8">
-            <div className="flex w-full justify-between">
-              <h1>Investments & Funding</h1>
-              <p>icon</p>
+          {cardData.map((card, index) => (
+            <div
+              key={index}
+              className="border-1 border-[rgba(251,198,159,0.8)] rounded-4xl px-8 py-10 bg-[linear-gradient(to_bottom_left,_#0B1219_60%,_#16202A_100%)]"
+            >
+              <div className="flex w-full justify-between">
+                <h1 className="text-2xl mb-10">{card.title}</h1>
+                <ArrowTopRightIcon width={25} height={25} />
+              </div>
+              <p>{card.description}</p>
             </div>
-            <p>
-              Placerat maecenas aliquam primis duis viverra integer. Vehicula
-              nulla bibendum facilisis per quis vehicula risus donec euismod.
-            </p>
-          </div>
-
-          <div className=" border-1 border-amber-500 rounded-4xl p-8">
-            <div className="flex w-full justify-between">
-              <h1>AI-Driven Data Centers</h1>
-              <p>icon</p>
-            </div>
-            <p>
-              Placerat maecenas aliquam primis duis viverra integer. Vehicula
-              nulla bibendum facilisis per quis vehicula risus donec euismod.
-            </p>
-          </div>
-
-          <div className="border-1 border-amber-500 rounded-4xl p-8">
-            <div className="flex w-full justify-between">
-              <h1>Office Spaces</h1>
-              <p>icon</p>
-            </div>
-            <p>
-              Placerat maecenas aliquam primis duis viverra integer. Vehicula
-              nulla bibendum facilisis per quis vehicula risus donec euismod.
-            </p>
-          </div>
+          ))}
         </div>
       </section>
     </div>
