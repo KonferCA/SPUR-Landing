b9b72398df95bfbdbbf80494df01a4f3f879f91d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-03-06 21:13:17 -0500

    [export] vexacool image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,5 +6,6 @@
 import SpurLogo from "./spurLogo.svg";
 import SpurBuilding from "./spurBuilding.png";
 import xLogo from "./xLogo.svg";
+import Vexacool from "./Vexacool.png";
 
-export { SpurLogo, SpurBuilding, xLogo };
+export { SpurLogo, SpurBuilding, xLogo, Vexacool };
